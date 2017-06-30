e908aef027d0ce65ef7b1d83259077c9396ac4c5
tkuchiki <kuchiki.taku@gmail.com>
2017-06-30 19:40:55 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@
 version=$(grep version plugin.yaml | awk '{print $2}')
 plugin_name=$(grep ^name plugin.yaml | awk '{print $2}')
 
-zip --exclude build.sh --exclude LICENSE --exclude README.md --exclude *.git* -r ${plugin_name}-${version}-plugin.zip .
+plugin_dir="${plugin_name}-${version}-plugin"
+mkdir -p ${plugin_dir}
+cp -a contents plugin.yaml ${plugin_dir}
+zip -r ${plugin_dir}.zip ${plugin_dir}
+rm -rf ${plugin_dir}
