f4a8adac168130fc4925b3e653bf0b6a6779b57f
tkuchiki <kuchiki.taku@gmail.com>
2017-07-03 16:57:31 +0900

    add config options

--- contents/consul_nodes.sh
+++ contents/consul_nodes.sh
@@ -2,9 +2,27 @@
 
 set -e
 
+SERVICE="${RD_CONFIG_SERVICE:-consul}"
+if [ "${SERVICE}" != "consul" -a "${SERVICE}" != "serf" ]; then
+    echo "config.service-name = (consul|serf)"
+    exit 1
+fi
+
+STATUSES="${RD_CONFIG_TARGET_STATUSES:-alive}"
+
+GREP=""
+
+if [ "${RD_CONFIG_INCLUDE_NODES}" != "" ]; then
+    GREP="| grep -E ${RD_CONFIG_INCLUDE_NODES}"
+fi
+
+if [ "${RD_CONFIG_EXCLUDE_NODES}" != "" ]; then
+    GREP="${GREP} | grep -v -E ${RD_CONFIG_EXCLUDE_NODES}"
+fi
+
 TMP_IFS=${IFS}
 IFS=$'\n'
-for line in $(consul members -status=alive | awk 'NR > 1 { print $1, $2 }' | awk -F ':' '{ print $1 }'); do
+for line in $(${SERVICE} members -status=${STATUSES} ${GREP} | awk 'NR > 1 { print $1, $2 }' | awk -F ':' '{ print $1 }'); do
     IFS=${TMP_IFS}
     set -- $line
     nodename="${1}"
